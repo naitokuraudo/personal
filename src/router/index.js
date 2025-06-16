c2ca38145e55e8682c2b28ef58d54c94e4e421c6
pursuit of dreams <59363567+GogaNotClown@users.noreply.github.com>
2025-06-16 09:51:08 +0300

    refactor(router): remove GogaNotClownView import and route

--- src/router/index.js
+++ src/router/index.js
@@ -4,14 +4,12 @@ import HomeView from '@/views/HomeView.vue'
 import ProjectsView from '@/views/ProjectsView.vue'
 import TeamView from '@/views/TeamView.vue'
 import ContactsView from '@/views/ContactsView.vue'
-import GogaNotClownView from '@/views/GogaNotClownView.vue'
 
 const routes = [
     {path: '/', name: 'home', component: HomeView},
     {path: '/projects', name: 'projects', component: ProjectsView},
     {path: '/team', name: 'team', component: TeamView},
     {path: '/contacts', name: 'contacts', component: ContactsView},
-    {path: '/team/goganotclown', name: 'team/goganotclown', component: GogaNotClownView},
 ]
 
 const router = createRouter({
