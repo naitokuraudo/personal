4fac36bd39692c817ad17706911e628e90183347
pursuit of dreams <59363567+GogaNotClown@users.noreply.github.com>
2025-06-16 09:50:33 +0300

    refactor(view): replace internal `/team/goganotclown` router-link with external GitHub link

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -7,9 +7,7 @@ import Header from '@/components/Header.vue'
   <section class="container mx-auto mt-6">
     <ul class="flex flex-col flex-wrap gap-3">
       <li>
-        <router-link class="link" to="/team/goganotclown">
-          pursuit of dreams
-        </router-link>
+        <a target="_blank" class="link" href="https://github.com/GogaNotClown">pursuit of dreams</a>
       </li>
       <li>
         <a target="_blank" class="link" href="https://github.com/arekusandorofu">amida</a>
